295c03acb686bce2e1947b6e14e62221f088d7ab
Alex Casson <alex.casson@manchester.ac.uk>
2025-02-22 13:42:22 +0000

    Removed font function at test

--- docs/_build/html/_static/uom_custom.js
+++ docs/_build/html/_static/uom_custom.js
@@ -1,40 +1,44 @@
-document.getElementById("uom_alt_font_button").addEventListener("click",
-    function(){
-        function get_font() {
-            var font;
-            console.log("H");
-            console.log(font);
-            try { font = localStorage.getItem('uom-sphinx-font'); } catch (e) { }
-            if (font === null || font === undefined) {
-                console.log("G");
-                return "font-roboto"; // default is hard coded
-            } else {
-                return font;
-            }
+(function fonts() {
+    var html = document.querySelector('html');
+    var fontToggleButton = document.getElementById('uom_alt_font_button');
+
+    fontToggleButton.addEventListener('click', function () {
+        set_font();
+    });
+
+    function get_font() {
+        var font;
+        try { font = localStorage.getItem('uom-sphinx-font'); } catch (e) { }
+        if (font === null || font === undefined) {
+            return "font-roboto"; // default is hard coded
+        } else {
+            return font;
         }
-    
-        function set_font(font, store = true) {
-            var previousFont = get_font();
-    
-            if (store) {
-                try { localStorage.setItem('uom-sphinx-font', font); } catch (e) { }
-            }
-    
-            if (font === previousFont) {
-                // no action needed
-            }  else if (font === "font-roboto") {
-                font = "font-opendyslexic";
-            } else if (font === "font-opendyslexic") {
-                font = "font-roboto";
-            } else 
-            {
-                // something has gone wrong! Set roboto as default fonr
-                font = "font-roboto";
-            }
-            html.classList.remove(previousFont);
-            html.classList.add(font);
+    }
+
+    function set_font(font, store = true) {
+        var previousFont = get_font();
+
+        if (store) {
+            try { localStorage.setItem('uom-sphinx-font', font); } catch (e) { }
         }
-        var font = get_font();
-        console.log(font);
-        set_font(font, false);
-})
+
+        if (font === previousFont) {
+            // no action needed
+        }  else if (font === "font-roboto") {
+            font = "font-opendyslexic";
+        } else if (font === "font-opendyslexic") {
+            font = "font-roboto";
+        } else 
+        {
+            // something has gone wrong! Set roboto as default fonr
+            font = "font-roboto";
+        }
+        html.classList.remove(previousFont);
+        html.classList.add(font);
+    }
+
+    var font = get_font();
+    set_font(font, false);
+
+})();
